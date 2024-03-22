1a65fa5512f3765bb224a0e474f1091c60c83766
Tomi <tomiatschool@gmail.com>
2024-03-22 13:45:20 +0100

    Add project files.

---
+++ Kalapacsvetess/Program.cs
@@ -0,0 +1,77 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Kalapacsvetess
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Sportolo> sportolok = new List<Sportolo>();
+            using (var sr = new StreamReader(@"..\..\..\src\kalapacsvetes.txt"))
+            {
+                _ = sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    sportolok.Add(new Sportolo(sr.ReadLine()));
+                }
+            }
+            Console.WriteLine($"4.feladat\t:{sportolok.Count()} dobás eredménye található ");
+
+            var magyarDobasok = sportolok.Where(s => s.orszagkod == "HUN").Select(s => s.eredmeny);
+            double atlagEredmeny = magyarDobasok.Average();
+
+            Console.WriteLine($"5.feladat\tA magyar sportolók átlaagosan {atlagEredmeny:F2} métert dobtak");
+
+            Console.WriteLine("6.feladat\tAdjon meg egy évszámot:");
+            if (int.TryParse(Console.ReadLine(), out int evszam))
+            {
+                var legjobbak = sportolok.Where(s => s.datum.Year == evszam).OrderByDescending(s => s.eredmeny);
+                if (legjobbak.Any())
+                {
+                    int legjobbakSzama = legjobbak.Count();
+                    Console.WriteLine($"{evszam}-ben/ban összesen {legjobbakSzama} dobás került be a legjobbak közé.");
+                    Console.WriteLine("Az alábbi sportolók érték el ezeket:");
+                    foreach (var sportolo in legjobbak)
+                    {
+                        Console.WriteLine($"- {sportolo.sportolo} ({sportolo.orszagkod}): {sportolo.eredmeny}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{evszam}-ben nem került be egy dobás eredménye sem a legjobbak közé.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Hibás évszám formátum.");
+            }
+            var legjobbakk = sportolok.OrderByDescending(s => s.eredmeny);
+            Dictionary<string, int> orszagStatisztika = new Dictionary<string, int>();
+
+            foreach (var sportolo in legjobbakk)
+            {
+                if (!orszagStatisztika.ContainsKey(sportolo.orszagkod))
+                {
+                    orszagStatisztika.Add(sportolo.orszagkod, 0);
+                }
+                orszagStatisztika[sportolo.orszagkod]++;
+            }
+
+            Console.WriteLine("\n7.feladat\tStatisztika:");
+            foreach (var kvp in orszagStatisztika.OrderByDescending(k => k.Value))
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} dobás");
+            }
+            var magyarEredmenyek = sportolok.Where(s => s.orszagkod == "HUN").ToList();
+            using (var sw = new StreamWriter(@"..\..\..\src\magyarok.txt"))
+            {
+                foreach (var sportolo in magyarEredmenyek)
+                {
+                    sw.WriteLine(sportolo.sportolo);
+                }
+            }
+        }
+    }
+}
+
---
+++ Kalapacsvetess/Sportolo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalapacsvetess
+{
+    internal class Sportolo
+    {
+        public int helyezes { get; set; }
+        public double eredmeny { get; set; }
+        public string sportolo { get; set; }
+        public string orszagkod { get; set; }
+        public string helyszin { get; set; }
+        public DateOnly datum { get; set; }
+        public Sportolo(string s)
+        {
+            string[] v = s.Split(";");
+            helyezes = int.Parse(v[0]);
+            eredmeny = double.Parse(v[1]);
+            sportolo= v[2];
+            orszagkod = v[3];
+            helyszin = v[4];
+            datum = DateOnly.Parse(v[5]);
+        }
+        public override string ToString()
+        {
+            return $"Helyezés:{helyezes}, Eredmény:{eredmeny}, Sportoló:{sportolo},Ország kódja:{orszagkod},Helyszín:{helyszin},Dátum:{datum}";
+        }
+    }
+}
